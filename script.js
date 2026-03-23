34eaa2dd99629db1367f459fc28bad92046c0f42
BV-Works <maildinerodeinternet@gmail.com>
2026-03-23 16:57:26 +0100

    set map 2, style both maps and pop ups

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-let map = L.map("map", {
-//   worldCopyJump: true,
-    maxBounds: [[-90, -180], [90, 180]],
-    maxBoundsViscosity: 1.0,
-    minZoom: 2,
-    maxZoom: 10
+let map1 = L.map("map1", {
+  maxBounds: [
+    [-90, -190],
+    [90, 190],
+  ],
+  maxBoundsViscosity: 1.0,
+  minZoom: 1,
+  maxZoom: 10,
 }).setView([20, 0], 2);
 
-let Esri_NatGeoWorldMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-	maxZoom: 16
-}).addTo(map);
+let map2 = L.map("map2", {
+  maxBounds: [
+    [-90, -190],
+    [90, 190],
+  ],
+  maxBoundsViscosity: 1.0,
+  minZoom: 1,
+  maxZoom: 10,
+}).setView([20, 0], 2);
 
+let Esri_NatGeoWorldMap1 = L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC",
+    maxZoom: 16,
+  },
+).addTo(map1);
 
-// //Agregar marcador
-// const marker = L.marker([51.5, -0.09])
-// .bindPopup("cheese house")
-// .addTo(map);
+let Esri_NatGeoWorldMap2 = L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC",
+    maxZoom: 16,
+  },
+).addTo(map2);
 
-// //Agregar popup
-// const popup = L.popup()
-//     .setLatLng([51.513, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
+map1.scrollWheelZoom.disable();
+map2.scrollWheelZoom.disable();
+// Habilitar solo si se mantiene Ctrl
+map1.on('wheel', function(e) {
+  if (e.originalEvent.ctrlKey) {
+    map1.scrollWheelZoom.enable();
+  } else {
+    map1.scrollWheelZoom.disable();
+  }
+});
+
+map2.on('wheel', function(e) {
+  if (e.originalEvent.ctrlKey) {
+    map2.scrollWheelZoom.enable();
+  } else {
+    map2.scrollWheelZoom.disable();
+  }
+});
+
 
 async function obtenerTerremotos() {
   const url =
@@ -40,7 +73,7 @@ async function obtenerTerremotos() {
       nombre: eq.properties.title,
       magnitud: eq.properties.mag,
       lugar: eq.properties.place,
-      tiempo: new Date(eq.properties.time),
+      fecha: new Date(eq.properties.time),
       coordenadas: {
         lat: eq.geometry.coordinates[1],
         lon: eq.geometry.coordinates[0],
@@ -58,18 +91,22 @@ async function obtenerTerremotos() {
   const terremotos = await obtenerTerremotos();
 
   terremotos.map((terremoto) => {
-    //Tratamiento de datos
     const coordenadas_terremoto = [
       terremoto.coordenadas.lat,
       terremoto.coordenadas.lon,
     ];
 
-    // Representación de datos
-    const marker = L.marker(coordenadas_terremoto)
-      .bindPopup(
-        `${terremoto.nombre}<br><button class='fav-btn'>Añadir a favoritos</button>`,
-      )
-      .addTo(map);
+    const color = getColor(terremoto.magnitud);
+    const marker = L.circleMarker(coordenadas_terremoto, {
+      radius: 3 + terremoto.magnitud,
+      fillColor: color,
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8,
+    })
+      .bindPopup(crearPopup(terremoto, color))
+      .addTo(map1);
 
     marker.on("popupopen", (e) => {
       const favButton = e.popup._contentNode.querySelector(".fav-btn");
@@ -82,3 +119,30 @@ async function obtenerTerremotos() {
   });
   console.log(terremotos);
 })();
+
+function getColor(mag) {
+  if (mag < 1) return "#9e9e9e";
+  if (mag < 2) return "#4caf50";
+  if (mag < 3) return "#1b5e20";
+  if (mag < 4) return "#ffeb3b";
+  if (mag < 5) return "#ffb74d";
+  if (mag < 6) return "#ef6c00";
+  if (mag < 7) return "#d32f2f";
+  return "#e91e63";
+}
+
+function crearPopup(terremoto, color) {
+  return `
+    <div class="quake-popup" style="background:${color}">
+      <h3 class="quake-title">🌍 Terremoto</h3>
+
+      <p><strong>Nombre:</strong> ${terremoto.nombre}</p>
+      <p><strong>Fecha del Evento:</strong> ${terremoto.fecha.toLocaleString()}</p>
+      <p><strong>Ubicación:</strong> ${terremoto.lugar}</p>
+      <p><strong>Código:</strong> ${terremoto.id}</p>
+      <p><strong>Magnitud:</strong> ${terremoto.magnitud}</p>
+
+      <button class="fav-btn">⭐ Añadir a favoritos</button>
+    </div>
+  `;
+}
